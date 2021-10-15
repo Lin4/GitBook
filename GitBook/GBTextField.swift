dbe92bd7941ce06dc79d481a28e9721b65a4b7e5
Lin4 <lingesus@gmail.com>
2021-10-15 13:51:54 -0400

    Custom Reusable objects,Subclassing, Dynamic Type,SemanticUI App, Custom Initializers

---
+++ GitBook/GBTextField.swift
@@ -0,0 +1,40 @@
+//
+//  GBTextField.swift
+//  GitBook
+//
+//  Created by Lingeswaran Kandasamy on 10/15/21.
+//
+
+import UIKit
+
+class GBTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configur()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func configur() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemGroupedBackground
+        autocorrectionType          = .no
+        
+        placeholder                 = "Enter a username"
+        
+    }
+
+}
